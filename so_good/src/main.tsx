6f27c0991a614f7c2ca8ab2f0c37c4318d118618
rvst312 <115166784+rvst312@users.noreply.github.com>
2024-08-09 13:34:52 +0200

    -definir la estructura de directorios -instalar "radixUI" libreria que usaremos para construir la ui -instalar "react-router" y configurar su ruta raiz

--- so_good/src/main.tsx
+++ so_good/src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { Theme, ThemePanel } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css';
+import App from './routes/App.tsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/* <Route path="login" element={<Login/>} />*/}
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <RouterProvider router={router} />
+      <ThemePanel />
+    </Theme>
   </React.StrictMode>,
 )
