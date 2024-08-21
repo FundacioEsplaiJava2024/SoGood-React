b9a8b7f51379b39073d920e1334bbc39411dd7b8
rvst312 <115166784+rvst312@users.noreply.github.com>
2024-08-21 11:02:02 +0200

    Pagina de usuario con sus componentes

--- so_good/src/components/Slider.tsx
+++ so_good/src/components/Slider.tsx
@@ -1,23 +1,106 @@
+import { Heading } from '@radix-ui/themes';
+import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
+
+interface SliderProps {
+    imgSrc: string,
+    title: string,
+    description: string,
+    price: number,
+}
 
 const Slider = () => {
 
-    const SliderWrapper = styled.section`
+    const products = [
+        {
+            id: "1",
+            imgUrl: "../public/food-bk.jpg",
+            productName: "bocadillo",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, blanditiis.",
+            direction: "c/ prueba, n 3",
+            quantity: 12,
+            price: 12.99,
+        },
+        {
+            id: "2",
+            imgUrl: "../public/food-bk.jpg",
+            productName: "hamburguesa",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, blanditiis.",
+            direction: "c/ prueba, n 3",
+            quantity: 12,
+            price: 12.99,
+        },
+        {
+            id: "3",
+            imgUrl: "../public/food-bk.jpg",
+            productName: "hamburguesa",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, blanditiis.",
+            direction: "c/ prueba, n 3",
+            quantity: 12,
+            price: 12.99,
+        },
+        {
+            id: "4",
+            imgUrl: "../public/food-bk.jpg",
+            productName: "hamburguesa",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, blanditiis.",
+            direction: "c/ prueba, n 3",
+            quantity: 12,
+            price: 12.99,
+        },
+        {
+            id: "5",
+            imgUrl: "../public/food-bk.jpg",
+            productName: "hamburguesa",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, blanditiis.",
+            direction: "c/ prueba, n 3",
+            quantity: 12,
+            price: 12.99,
+        },
+    ]
 
+    const SliderWrapper = styled.section`
+        display: flex;
+        justify-content: start;
+        align-items: center;
+        overflow-x: scroll;
+        gap: 20px;
+        margin-top: 20px;
+        margin-bottom: 3rem;
+        padding-left: 3rem;
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+        padding-right: 3rem;
     `;
 
     return (
-        <SliderWrapper>
-            <SliderItem />
-        </SliderWrapper>
+        <>
+            <Heading style={{ paddingLeft: "3rem", marginTop: "3rem" }}>
+                Categorie
+            </Heading>
+            <SliderWrapper>
+                {products.map((product) => (
+                    <SliderItem
+                        key={product.id}
+                        imgSrc={product.imgUrl}
+                        title={product.productName}
+                        price={product.price}
+                        description={product.description}
+                    />
+                ))}
+            </SliderWrapper>
+        </>
     )
 }
 export default Slider;
 
-const SliderItem = () => {
+
+const SliderItem: React.FC<SliderProps> = ({ imgSrc, title, price, description }) => {
 
     const SliderItem = styled.div`
-
+        background: #fff;
+        border-radius:10px;
+        box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px;
     `
 
     const SliderTop = styled.div`
@@ -25,19 +108,55 @@ const SliderItem = () => {
     `;
 
     const SliderBottom = styled.div`
+        padding: 20px; 
+    `;
+
+    const Image = styled.img`
+        max-width: 400px;
+        object-fit: cover;
+        border-radius: 10px 10px 0px 0px;
+    `;
+
+    const ItemsWrapper = styled.div`
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    `;
+
+    const Title = styled.h3`
+
+    `
+
+    const Price = styled.span`
+
+    `;
+
+    const Description = styled.p`
 
     `;
 
     return (
         <>
-            <SliderItem>
-                <SliderTop>
-
-                </SliderTop>
-                <SliderBottom>
-
-                </SliderBottom>
-            </SliderItem>
+            <Link to={`/product/${title}`}>
+                <SliderItem>
+                    <SliderTop>
+                        <Image src={imgSrc} />
+                    </SliderTop>
+                    <SliderBottom>
+                        <ItemsWrapper>
+                            <Title>
+                                {title}
+                            </Title>
+                            <Price>
+                                {price}
+                            </Price>
+                        </ItemsWrapper>
+                        <Description>
+                            {description}
+                        </Description>
+                    </SliderBottom>
+                </SliderItem>
+            </Link>
         </>
     )
 }
--- so_good/src/pages/UserMode.tsx
+++ so_good/src/pages/UserMode.tsx
@@ -1,7 +1,9 @@
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import { TextField } from '@radix-ui/themes';
+import { Button } from '@radix-ui/themes/src/index.js';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import Slider from '../components/Slider';
 
 function UserMode() {
 
@@ -19,24 +21,33 @@ function UserMode() {
     const { profile } = useParams();
 
     const HeroName = styled.section`
-
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin: 20px;
     `;
 
     const SearchWrapper = styled.div`
-
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
     `;
 
     const Search = styled.div`
-
+        max-width: 1150px;
+        width: 100%;
     `;
 
     const Categories = styled.div`
-
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        gap: 20px;
     `;
-
-    const Category = styled.button`
-
-    `
 
     return (
         <>
@@ -53,12 +64,15 @@ function UserMode() {
                 </Search>
                 <Categories>
                     {categories.map((category)=>(
-                        <Category key={category}>
+                        <Button key={category} style={{ width: "100px" }}>
                             {category}
-                        </Category>
+                        </Button>
                     ))}
                 </Categories>
             </SearchWrapper>
+            <Slider/>
+            <Slider/>
+            <Slider/>
         </>
 
     )
